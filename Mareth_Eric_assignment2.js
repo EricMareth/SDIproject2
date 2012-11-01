8565576384784b5a288d9bb188cb36ee0fe66217
EricMareth <EricMareth@fullsail.edu>
2012-10-31 23:47:44 -0400

    Trying to add changes to github.

--- Mareth_Eric_assignment2.js
+++ Mareth_Eric_assignment2.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+//Variables
+var sisters = [ "Jenna", "Mary", "Katie", "Lizzie" ];
+var ticklishSpots = [ "feet", "under her arms", "neck" ];
+var cry = "\"Uncle, " + sisters[0] + ", UNCLE!\"";
+var taunt = "\"HA HA, "+ sisters[0] + " is a stupid-head!\"";
+
+//Number of seconds tickled determines success or failure.
+var tickleFight = function(seconds){
+		for (tickledSecs = 0; tickledSecs < seconds; tickledSecs +=5) {
+			console.log("I have tickled " + sisters[1] + " for " + tickledSecs + " seconds already! When will she give up?")
+		};
+	console.log(sisters[1] + " yells " + cry)
+};
+
+tickleFight(30);
